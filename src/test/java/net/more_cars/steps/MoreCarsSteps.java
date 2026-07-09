24c80233e46f61ce096a2a2bdc7e9440491eb376
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-07-09 17:22:41 +0200

    simplifying test step implementation

--- src/test/java/net/more_cars/steps/MoreCarsSteps.java
+++ src/test/java/net/more_cars/steps/MoreCarsSteps.java
@@ -5,7 +5,6 @@ import io.cucumber.java.en.When;
 import net.more_cars.driver.DriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -29,15 +28,9 @@ public class MoreCarsSteps {
 
     @Then("the page should contain the following sections")
     public void the_page_should_contain_the_following_sections(List<String> sectionNames) {
-        List<String> actualSectionTexts = driver.findElements(By.cssSelector("section h2"))
-                .stream()
-                .map(WebElement::getText)
-                .toList();
-
         for (String sectionName : sectionNames) {
-            boolean found = actualSectionTexts.stream()
-                    .anyMatch(text -> text.contains(sectionName));
-            assertTrue(found, "Page should contain section: " + sectionName);
+            By locator = By.xpath("//section//h2[contains(., '" + sectionName + "')]");
+            assertTrue(driver.findElements(locator).size() > 0, "Missing section: " + sectionName);
         }
     }
 }
