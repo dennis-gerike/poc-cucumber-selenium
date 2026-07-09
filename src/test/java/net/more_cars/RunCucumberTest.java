7474a2869726aed3913a0b57444083229bab0b35
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-07-09 17:40:09 +0200

    fixing config error

--- src/test/java/net/more_cars/RunCucumberTest.java
+++ src/test/java/net/more_cars/RunCucumberTest.java
@@ -2,14 +2,14 @@ package net.more_cars;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectPackages("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "net.more_cars.steps")
 public class RunCucumberTest {
 }
